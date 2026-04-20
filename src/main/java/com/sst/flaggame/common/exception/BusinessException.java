2d81bcb5786e00841d9ccc42c397c6a330ea6cd0
tlsdla1235 <tlsdla1235@uos.ac.kr>
2026-04-20 09:06:38 +0900

    feat: global exception handler

--- src/main/java/com/sst/flaggame/common/exception/BusinessException.java
+++ src/main/java/com/sst/flaggame/common/exception/BusinessException.java
@@ -4,7 +4,10 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
-class BusinessException {
+public class BusinessException extends RuntimeException{
     private final ErrorCode errorCode;
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
 }
